6f4947f863c39c41a7f560541b22d2e8a46262f3
gaurav.prasad <gaurav.prasad@flipkart.com>
2019-02-05 19:26:18 +0530

    Adding More ro-bust script

--- grpc-jexpress-template/generate_descriptor_set.sh
+++ grpc-jexpress-template/generate_descriptor_set.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+#set -x
 mvn clean install
 rm -rf *.pb
-proto_dependencies=$(ls -1 target/protoc-dependencies/*|head -n1|cut -d":" -f1)
-protoc -I.  -I${proto_dependencies} -Isrc/main/proto --include_imports  --include_source_info  --descriptor_set_out=sample_proto_descriptor_set.pb  src/main/proto/userservice.proto
+proto_dependencies=$(ls -1 target/protoc-dependencies/*|grep target|cut -d":" -f1|sed s/^/-I/g|tr '\n' ' ')
+protoc -I.  ${proto_dependencies} -Isrc/main/proto --include_imports  --include_source_info  --descriptor_set_out=sample_proto_descriptor_set.pb  src/main/proto/userservice.proto
