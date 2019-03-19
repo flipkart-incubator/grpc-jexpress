4272e37012f5156f4ec7bff90b1a9169cf10517f
anand.pandey <anand.pandey@flipkart.com>
2019-03-19 15:42:43 +0530

    Added copyright statements

--- core/src/test/java/com/flipkart/gjex/core/config/ArgumentParserWrapperTest.java
+++ core/src/test/java/com/flipkart/gjex/core/config/ArgumentParserWrapperTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) The original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.flipkart.gjex.core.config;
 
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
@@ -25,8 +40,8 @@ public class ArgumentParserWrapperTest {
 
     @Test
     public void getConfigServiceUrlYmlFilePath() throws ArgumentParserException {
-        String[] arguments = {"server", "config-svc://10.47.0.101:80/bucket-name"};
+        String[] arguments = {"server", "config-svc://10.47.0.11:80/bucket-name"};
         Namespace namespace = parser.parseArguments(arguments);
-        assertThat(namespace.get("file").equals("config-svc://10.47.0.101:80/bucket-name"));
+        assertThat(namespace.get("file").equals("config-svc://10.47.0.11:80/bucket-name"));
     }
 }
