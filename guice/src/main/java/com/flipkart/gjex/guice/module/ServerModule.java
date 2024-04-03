9f6e26c1fd793ebd53fb60552a062390f9df5f84
ajay.jalgaonkar <ajay.jalgaonkar@flipkart.com>
2024-04-03 16:25:11 +0530

    binding HealthGrpc.HealthImplBase to GrpcHealthCheckService

--- guice/src/main/java/com/flipkart/gjex/guice/module/ServerModule.java
+++ guice/src/main/java/com/flipkart/gjex/guice/module/ServerModule.java
@@ -28,6 +28,7 @@ import com.flipkart.gjex.grpc.service.ScheduledJobManager;
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 import io.grpc.BindableService;
+import io.grpc.health.v1.HealthGrpc;
 
 /**
  * <code>ServerModule</code> is a Guice {@link AbstractModule} implementation used for configuring the Grpc Server and Dashboard server.
@@ -43,7 +44,7 @@ public class ServerModule extends AbstractModule {
 		bind(TracingInterceptor.class).annotatedWith(Names.named("TracingInterceptor")).to(TracingInterceptor.class);
 		bind(StatusMetricInterceptor.class).annotatedWith(Names.named("StatusMetricInterceptor")).to(StatusMetricInterceptor.class);
 		bind(Service.class).annotatedWith(Names.named("GrpcServer")).to(GrpcServer.class);
-		bind(BindableService.class).annotatedWith(Names.named("GrpcHealthCheckService")).to(GrpcHealthCheckService.class);
+		bind(HealthGrpc.HealthImplBase.class).annotatedWith(Names.named("GrpcHealthCheckService")).to(GrpcHealthCheckService.class);
 		bind(Service.class).annotatedWith(Names.named("DashboardServer")).to(DashboardServer.class);
 		bind(Service.class).annotatedWith(Names.named("APIServer")).to(ApiServer.class);
 		bind(Service.class).annotatedWith(Names.named("ScheduleJobManager")).to(ScheduledJobManager.class);
