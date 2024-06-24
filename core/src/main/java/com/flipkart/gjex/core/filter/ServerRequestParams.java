568fd4e3e1a5415d97c61b652e99e948ceaf1591
ajay.jalgaonkar <ajay.jalgaonkar@flipkart.com>
2024-06-24 16:18:05 +0530

    updated author

--- core/src/main/java/com/flipkart/gjex/core/filter/ServerRequestParams.java
+++ core/src/main/java/com/flipkart/gjex/core/filter/ServerRequestParams.java
@@ -3,6 +3,12 @@ package com.flipkart.gjex.core.filter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Parameters for passing to filters
+ * @author ajay.jalgaonkar
+ *
+ */
+
 @AllArgsConstructor
 @Getter
 public class ServerRequestParams {
