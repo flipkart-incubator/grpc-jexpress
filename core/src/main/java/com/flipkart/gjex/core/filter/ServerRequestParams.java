f650e89866b91991353ff83e942cb6b2fe971832
ajay.jalgaonkar <ajay.jalgaonkar@flipkart.com>
2024-06-24 12:47:02 +0530

    not using lombok

--- core/src/main/java/com/flipkart/gjex/core/filter/ServerRequestParams.java
+++ core/src/main/java/com/flipkart/gjex/core/filter/ServerRequestParams.java
@@ -1,11 +1,19 @@
 package com.flipkart.gjex.core.filter;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public class ServerRequestParams {
   private String clientIp;
   private String methodName;
+
+  public ServerRequestParams(String clientIp, String methodName) {
+    this.clientIp = clientIp;
+    this.methodName = methodName;
+  }
+
+  public String getClientIp() {
+    return clientIp;
+  }
+
+  public String getMethodName() {
+    return methodName;
+  }
 }
