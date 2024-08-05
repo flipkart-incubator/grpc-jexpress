1b3952a982abac04b3fddff0f2e97fb6e9999050
ajaypj <ajay.jalgaonkar@flipkart.com>
2024-08-05 21:50:03 +0530

    access log

--- core/src/main/java/com/flipkart/gjex/core/filter/grpc/AccessLogGrpcFilter.java
+++ core/src/main/java/com/flipkart/gjex/core/filter/grpc/AccessLogGrpcFilter.java
@@ -87,6 +87,8 @@ public class AccessLogGrpcFilter<R extends GeneratedMessageV3, S extends Generat
                       .responseTime(System.currentTimeMillis()-startTime)
                           .build();
     logger.info(accessLogContext.format());
+
+
   }
 
   /**
