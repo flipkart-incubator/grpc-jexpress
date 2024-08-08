aaa4a64706336a9f1092a72fd1a66605f980061b
naman.soni <naman.soni@flipkart.com>
2024-08-08 18:13:11 +0530

    added test case

--- guice/src/test/java/com/flipkart/gjex/grpc/interceptor/FilterInterceptorTest.java
+++ guice/src/test/java/com/flipkart/gjex/grpc/interceptor/FilterInterceptorTest.java
@@ -3,15 +3,16 @@ package com.flipkart.gjex.grpc.interceptor;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
 public class FilterInterceptorTest {
-    private static final String IP_ADDRESS = "1.2.3.4";
+    private static final String IP_ADDRESS = "0.0.0.0";
 
     @Test
     public void getClientIpTest(){
-        InetSocketAddress inetSocketAddress = new InetSocketAddress(IP_ADDRESS, 1234);
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(1234);
         Assert.assertEquals(IP_ADDRESS, FilterInterceptor.getClientIp(inetSocketAddress));
 
         SocketAddress socketAddress = new SocketAddress() {
