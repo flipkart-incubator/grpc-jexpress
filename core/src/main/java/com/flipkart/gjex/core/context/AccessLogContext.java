57d4307d01687485c20809bbeba29d4f5e597542
Kinshuk Bairagi <hi@kinsh.uk>
2024-08-08 10:52:22 +0530

    minor changes

--- core/src/main/java/com/flipkart/gjex/core/context/AccessLogContext.java
+++ core/src/main/java/com/flipkart/gjex/core/context/AccessLogContext.java
@@ -8,7 +8,6 @@ import org.apache.commons.text.StringSubstitutor;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
