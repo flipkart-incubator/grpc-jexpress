090c832a703825604bb3066940fd31e301ddac71
Kinshuk Bairagi <hi@kinsh.uk>
2024-08-09 16:49:48 +0530

    Update HttpFilterInterceptorTest.java

--- guice/src/test/java/com/flipkart/gjex/http/interceptor/HttpFilterInterceptorTest.java
+++ guice/src/test/java/com/flipkart/gjex/http/interceptor/HttpFilterInterceptorTest.java
@@ -15,8 +15,7 @@ import org.junit.Test;
 
 import javax.servlet.http.HttpServletRequest;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
